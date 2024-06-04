4ba6bbaade9722c4494810f86d5f273b65d06bd1
me-cfs <168860247+me-cfs@users.noreply.github.com>
2024-06-04 19:20:17 +0200

    Update script.js

--- news/script.js
+++ news/script.js
@@ -1,3 +1,5 @@
+import { formatDate } from './utils.js';
+
 const rssFeeds = [
     'https://thesicktimes.org/feed/',
     'https://politepol.com/fd/yNgKhc4c7HHu.xml',
@@ -80,24 +82,6 @@ async function loadFeeds() {
     } else {
         loadMoreButton.style.display = 'block';
     }
-}
-
-function formatDate(date) {
-    const options = { day: 'numeric', month: 'long', year: 'numeric' };
-    const formattedDate = date.toLocaleDateString('en-GB', options);
-
-    // Add suffix to day
-    const day = date.getDate();
-    let suffix = 'th';
-    if (day === 1 || day === 21 || day === 31) {
-        suffix = 'st';
-    } else if (day === 2 || day === 22) {
-        suffix = 'nd';
-    } else if (day === 3 || day === 23) {
-        suffix = 'rd';
-    }
-
-    return formattedDate.replace(/\d+/, day + suffix);
 }
 
 // Add event listener to the "Load More" button
