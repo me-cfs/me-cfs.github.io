fa3f8140b474eda3009c75608b02d58aa5416f17
me-cfs <168860247+me-cfs@users.noreply.github.com>
2024-06-24 14:58:05 +0200

    Update test.js

--- news/rss/test.js
+++ news/rss/test.js
@@ -1,68 +1,9 @@
 const Parser = require('rss-parser');
 const fs = require('fs');
 const xml2js = require('xml2js');
+const feedUrls = require('community_input'); // Adjust the path as necessary
 
 const parser = new Parser();
-const feedUrls = [
-  { 
-    url: 'https://med-mastodon.com/@s4me.rss',
-    name: 'Science for ME', 
-    cutoffDate: new Date('2024-05-01'),
-    exclusionWords: [],
-    inclusionWords: ['News in Brief'],
-    undefinedTitle: 'News in Brief for the week ending, ' + getOneWeekAgoDate(),
-    getContentLink: 'https://www.s4me.info'
-  },
-  { 
-    url: 'https://thesicktimes.org/feed/', 
-    name: 'The Sick Times', 
-    cutoffDate: new Date('2024-05-01'),
-    exclusionWords: ["National Covid-19 trends", "Research updates"]
-  },
-  { 
-    url: 'https://politepol.com/fd/yNgKhc4c7HHu.xml', 
-    name: 'Virology', 
-    cutoffDate: new Date('2024-05-29'),
-    exclusionWords: []
-  },
-  { 
-    url: 'https://politepol.com/fd/uu29e43WxMVl.xml', 
-    name: 'ME/CFS Research Review', 
-    cutoffDate: new Date('2024-05-30'),
-    exclusionWords: []
-  },
-  { 
-    url: 'https://politepol.com/fd/jyPhRAUUwOqs.xml', 
-    name: 'Science for ME', 
-    cutoffDate: new Date('2024-05-30'),
-    exclusionWords: []
-  },
-  { 
-    url: 'http://fetchrss.com/rss/6663490de2588464cc0e7843666348e357063cb2e901eac3.xml', 
-    name: 'The Canary', 
-    cutoffDate: new Date('2024-06-08'),
-    exclusionWords: []
-  },
-  { 
-    url: 'https://politepol.com/fd/5l40hlTQDRT0.xml', 
-    name: 'The Science Bit', 
-    cutoffDate: new Date('2024-06-06'),
-    exclusionWords: []
-  },
-  { 
-    url: 'https://www.healthrising.org/feed/', 
-    name: 'HealthRising', 
-    cutoffDate: new Date('2024-05-01'),
-    exclusionWords: ["Recovery", "Exercise Program"]
-  },
-  { 
-    url: 'https://politepol.com/fd/lBx0N55ObPbU.xml', 
-    name: 'Thoughts about ME', 
-    cutoffDate: new Date('2024-06-15'),
-    exclusionWords: []
-  },
-  // Add more feeds with their respective cutoff dates and exclusion words as needed
-];
 const localFile = 'news/rss/test.xml'; // Correct path to your XML file
 
 function getOneWeekAgoDate() {
