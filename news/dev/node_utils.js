0cbaeb817991599bb387098a35a9583d533ae128
me-cfs <168860247+me-cfs@users.noreply.github.com>
2024-06-30 14:25:38 +0200

    Update node_utils.js

--- news/dev/node_utils.js
+++ news/dev/node_utils.js
@@ -1,10 +1,35 @@
 // utils file in node.js export language -- urgh JS is confusing
 
+
 function getOneWeekAgoDate() {
   const date = new Date();
   date.setDate(date.getDate() - 7);
-  const options = { year: 'numeric', month: 'long', day: 'numeric' };
-  return date.toLocaleDateString('en-US', options);
+
+  const options = { month: 'long', day: 'numeric' };
+  const dateString = date.toLocaleDateString('en-US', options);
+
+  const day = date.getDate();
+  let suffix;
+
+  if (day >= 11 && day <= 13) {
+    suffix = 'th';
+  } else {
+    switch (day % 10) {
+      case 1:
+        suffix = 'st';
+        break;
+      case 2:
+        suffix = 'nd';
+        break;
+      case 3:
+        suffix = 'rd';
+        break;
+      default:
+        suffix = 'th';
+    }
+  }
+
+  return dateString.replace(day, day + suffix);
 }
 
 module.exports = {
