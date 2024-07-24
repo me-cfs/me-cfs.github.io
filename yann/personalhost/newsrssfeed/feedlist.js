58eb3b5434df694be21d2a092113a4c88029acc5
me-cfs <168860247+me-cfs@users.noreply.github.com>
2024-07-24 15:30:14 +0200

    Update feedlist.js

--- yann/personalhost/newsrssfeed/feedlist.js
+++ yann/personalhost/newsrssfeed/feedlist.js
@@ -81,7 +81,7 @@ module.exports = [
     off: false,
     exclusionWords: ['Hurricane', 'Tropical Storm',
       'Paramount', 'Skydance', 'Fact Check: Biden',
-      'fans',
+      'fans', 'collection:', 'peggy',
     ],
   },
   { 
