b118b2a9dadf431652e8a622b6951f22ae42ece2
me-cfs <168860247+me-cfs@users.noreply.github.com>
2024-07-26 16:43:54 +0200

    Update feedlist.js

--- yann/personalhost/newsrssfeed/feedlist.js
+++ yann/personalhost/newsrssfeed/feedlist.js
@@ -44,6 +44,7 @@ module.exports = [
     name: 'WorldPost (Huffington)',
     url: 'https://politepol.com/fd/JvzTMgPr0nym.xml',
     off: false,
+    exclusionWords: ['Hilarious'],
   },
   { 
     name: 'Monbiot Guardian',
