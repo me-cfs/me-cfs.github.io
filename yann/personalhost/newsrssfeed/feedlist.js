b4e01fd769e93646298e9664520d36aca41af324
me-cfs <168860247+me-cfs@users.noreply.github.com>
2024-12-12 21:24:37 +0100

    Update feedlist.js

--- yann/personalhost/newsrssfeed/feedlist.js
+++ yann/personalhost/newsrssfeed/feedlist.js
@@ -5,7 +5,7 @@ const clickbait = ['hilarious', 'most beautiful', 'rage', 'GOAT', 'glow up', 'sh
 ];
 // Hill (Healthcare)
 const fluffyTitle = ['spirit of', 'the tragedy of', 'just gave us', 'grandma', 'behind the curtain', 'the most powerful',
-    'and the prize for the',
+    'and the prize for the', 'sandwich problem'
 ];
 // Axios, Economist, NYT Most Viewed, Politico EU
 const nonArticle = ['podcast', 'quiz', 'video'];
