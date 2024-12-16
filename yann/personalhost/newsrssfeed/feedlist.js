bc2d326f2876059f4dc7aa1bf070492a73494fa8
me-cfs <168860247+me-cfs@users.noreply.github.com>
2024-12-16 12:30:05 +0100

    Update feedlist.js

--- yann/personalhost/newsrssfeed/feedlist.js
+++ yann/personalhost/newsrssfeed/feedlist.js
@@ -14,7 +14,7 @@ const nonInformativeTitle = ['how much', '?', 'pour vous?', 'what to know', 'wha
     'how it happened', 'know about', 'who is', 'why',
 ];
 // Axios. Hill Climate, Hill Healthcare. Huffpost (World), NYT Most Viewed, Vox
-const singleCrime = ['assault', 'rape', 'guilty', 'charges with', 'alcohol poisoning', 'apologise'];
+const singleCrime = ['assault', 'rape', 'guilty', 'charges with', 'alcohol poisoning', 'apologise', 'smuggler'];
 // -BBC Australia, -BBC Fiji
 const spanish = [
   'noticias', 'artículo', 'contenido', 'actualidad', 'español',
