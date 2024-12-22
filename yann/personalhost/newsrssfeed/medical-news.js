0b518318f7ffbff47e253488b6034ef51b837b3b
me-cfs <168860247+me-cfs@users.noreply.github.com>
2024-12-22 14:47:48 +0100

    Update medical-news.js

--- yann/personalhost/newsrssfeed/medical-news.js
+++ yann/personalhost/newsrssfeed/medical-news.js
@@ -4,5 +4,12 @@ module.exports = [
     url: 'https://politepol.com/fd/1d2wlU3F2Cw5.xml',
     off: false,
     exclusionWords: ['?'],
+    cutoffDate: new Date('2024-12-25')
+  },
+  {
+    name: 'The Disabled Writer',
+    url: 'https://politepol.com/fd/mnINOeCIaxAh.xml',
+    inclusionWords: ['Caretaker', ...disability, 'language', ...medicine, 'symptom'],
+    cutoffDate: new Date('2024-12-11')
   },
 ];
