b7ce7b9ee25f753ed56ba13d38aad68490d6ee01
me-cfs <168860247+me-cfs@users.noreply.github.com>
2024-12-24 10:37:59 +0100

    Update topical-lists.js

--- yann/personalhost/newsrssfeed/topical-lists.js
+++ yann/personalhost/newsrssfeed/topical-lists.js
@@ -43,7 +43,7 @@ const triggers = ['gender dysphoria', 'Yuppie Flu'];
 const uninterestedGeneral = ['bouchon', 'dances', 'Taylor Swift', 'musical', 'Christmas', 'Baby Jesus', 'manger sain', 'un hôtel',
     'fashion brand', 'traffic jam', 'noël',
 ];
-// -Axios, -BBC (Australia), -Hurfington Post (World Post), -NYT Most Viewed, -RTS
+// -Axios, -BBC (Australia), -Hurfington Post (World Post), -NYT Most Viewed, -Politico EU, -RTS
 const uninterestedPeople = ['Taylor Swift', 'Austin Tice', 'Juan Sotos', 'Jake Paul', 'Mike Tyson'];
 // Axios
 const uninterestedSports = ['boxing', 'USWNT', 'LeBron', 'NBA', 'NFL', 'college football', 'Mets', 'cricket', 'NY Yankees', 'martial arts'];
