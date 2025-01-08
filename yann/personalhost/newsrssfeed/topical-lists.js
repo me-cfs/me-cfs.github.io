e038b6671106716cf679e2f5b13e86b57783bd40
me-cfs <168860247+me-cfs@users.noreply.github.com>
2025-01-08 17:01:11 +0000

    Update topical-lists.js

--- yann/personalhost/newsrssfeed/topical-lists.js
+++ yann/personalhost/newsrssfeed/topical-lists.js
@@ -25,7 +25,7 @@ const us = ['Anchorage', ...usPolitics];
 //exclusionLists
 const clickbait = ['hilarious', 'most beautiful', 'rage', 'GOAT', 'glow up', 'shout', 'shitstir',
   'crazy', 'apocalypse', 'mega-maga', 'seeth', 'shocking', "won't believe", 'truth about',
-];l
+];
 // -Crikey, -Hill (Healthcare), -Politico EU
 const culture = ['anime', 'artist', 'manga', 'oscar', 'film', 'celebrity', 'gossip', 'entertainment',];
 // -Open Democracy, -RTS, -Semafor
