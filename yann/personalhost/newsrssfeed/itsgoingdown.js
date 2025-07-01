fc95c6d1fdaf5a722a0cdea36e8c592b4ba10d32
me-cfs <168860247+me-cfs@users.noreply.github.com>
2025-07-01 13:15:03 +0200

    Update itsgoingdown.js

--- yann/personalhost/newsrssfeed/itsgoingdown.js
+++ yann/personalhost/newsrssfeed/itsgoingdown.js
@@ -1,4 +1,9 @@
-module.exports {
-    
+module.exports = [
+    {
+    name: "It's Going Down",
+    url: 'https://itsgoingdown.org/feed/',
+    exclusionWords: ["Dugout"],
+    off: false,
+  },
 
-}
+];
