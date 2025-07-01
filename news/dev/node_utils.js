51b324baa412f09f7a915e2bf134010a217d7097
me-cfs <168860247+me-cfs@users.noreply.github.com>
2025-07-01 13:31:58 +0200

    Update node_utils.js

--- news/dev/node_utils.js
+++ news/dev/node_utils.js
@@ -11,6 +11,9 @@ const parser = new Parser({
   requestOptions: {
     headers: {
       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115 Safari/537.36',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      'Accept-Language': 'en-US,en;q=0.5',
+      'Connection': 'keep-alive',
     },
   },
 });
